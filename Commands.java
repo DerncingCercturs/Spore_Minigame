3e7b879c10f20eb51d4806c36ab67a8729a08d43
Ryan Boehm <rynoraja@shaw.ca>
2013-06-25 18:33:59 -0500

    Update Commands.java
    Edited parsing

--- Commands.java
+++ Commands.java
@@ -8,16 +8,16 @@ import org.bukkit.entity.Player;
 public class Commands implements CommandExecutor {
 
   public Commands(SporeMinigame sporeMinigame) {
-		// TODO Auto-generated constructor stub
+	// TODO Auto-generated constructor stub
 	}
 
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		Player player = (Player) sender;
+	Player player = (Player) sender;
     	if(cmd.getName().equalsIgnoreCase("stest")){
-			player.sendMessage(enabled);
-			//broken until we can find out how to make value from config static
-		}
+		player.sendMessage(enabled);
+		//broken until we can find out how to make value from config static
+	}
     	return false;
     }
 }
